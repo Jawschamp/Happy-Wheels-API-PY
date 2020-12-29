0d19eea25bdbec6e698a70f11eeb6024eb72f784
Jaws <38185563+Jawschamp@users.noreply.github.com>
2020-12-29 04:30:08 -0800

    Update and rename Test_Levels.py to Testing.py

---
+++ Testing.py
@@ -0,0 +1,44 @@
+import requests
+import xmltodict
+import json
+
+class Translator:
+    class LevelTranslator:
+        date = "dp"
+        level_name = "in"
+        author = "un"
+        disc = "uc"
+        player_count = "pc"
+        level_rating = "rg"
+
+
+class Level:
+    search = "http://totaljerkface.com/get_level.hw"
+
+    def search_level_by_level_name(name_of_lvl):
+        """'sortby` allows for search by newest or oldest"""
+        payload = {
+            "action": "search_by_name",
+            "page": "1",
+            "sortby": "newest",
+            "uploaded": "week",
+            "sterm": name_of_lvl
+        }
+        request = requests.request("POST", url=Level.search, data=payload).content
+        jsondict = xmltodict.parse(request)
+        with open("Test.json", "w") as f:
+            json.dump(jsondict, f)
+        return jsondict
+
+class Author:
+    def search_author(author_name):
+        payload = {
+            "action": "search_by_user",
+            "page": "1",
+            "sortby": "newest",
+            "uploaded": "week",
+            "sterm": author_name
+        }
+        request = requests.post(url=Level.search, data=payload).content
+        jsondict = xmltodict.parse(request)
+        return jsondict
